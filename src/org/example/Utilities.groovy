3477b131bfe8afbd39e983d3a0f1faf4b8b4c1ba
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-08 02:06:12 +0530

    Update Utilities.groovy

--- src/org/example/Utilities.groovy
+++ src/org/example/Utilities.groovy
@@ -2,14 +2,14 @@ package org.example
 
 class Utilities {
     static String getScannerHome() {
-        return tool name: 'SonarQube-Scanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+        return tool(name: 'SonarQube-Scanner', type: 'hudson.plugins.sonar.SonarRunnerInstallation')
     }
 
     static void runSonarScanner(String scannerHome, String projectKey, String sourceDir, String sonarHostUrl, String sonarToken) {
-        sh "${scannerHome}/bin/sonar-scanner " +
-           "-Dsonar.projectKey=${projectKey} " +
-           "-Dsonar.sources=${sourceDir} " +
-           "-Dsonar.host.url=${sonarHostUrl} " +
-           "-Dsonar.login=${sonarToken}"
+        sh """${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.sources=${sourceDir} \
+            -Dsonar.host.url=${sonarHostUrl} \
+            -Dsonar.login=${sonarToken}"""
     }
 }
