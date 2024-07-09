391e329026f15b0388c89ec69bf7db954f999c10
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 03:41:30 +0530

    Update CoveragePython.groovy

--- src/CoveragePython.groovy
+++ src/CoveragePython.groovy
@@ -1,26 +1,16 @@
-// vars/setupVirtualEnvironment.groovy
-
 def call() {
-    node {
-        stage('Set Up Virtual Environment') {
-            checkout scm
-
-            // Set up virtual environment
-            def pythonHome = tool name: 'Python', type: 'hudson.plugins.python.PythonInstallation'
-            def pipHome = tool name: 'Python', type: 'hudson.plugins.python.PythonInstallation'
-            env.PYTHON_HOME = pythonHome
-            env.PATH = "${env.PYTHON_HOME}/bin:${env.PATH}"
-            sh 'python3 -m venv myenv'
-            sh '. myenv/bin/activate'
-            
-            // Install dependencies
-            sh 'pip install -r requirements.txt'
-            sh 'pip install coverage'
-            sh 'pip install pytest'
-
-            // Run tests
-            sh 'coverage run -m pytest'
-            sh 'coverage report -m'
-        }
-    }
+    def workspaceDir = "${WORKSPACE}"
+    echo "Current directory: ${pwd()}"
+    echo "Workspace directory: ${workspaceDir}"
+    sh """
+        ls -la ${workspaceDir}
+        python3 -m venv myenv
+        . myenv/bin/activate
+        echo "Workspace directory: ${workspaceDir}"
+        pip install -r ${workspaceDir}/requirements.txt
+        pip install coverage
+        pip install pytest
+        coverage run -m pytest
+        coverage report -m
+    """
 }
