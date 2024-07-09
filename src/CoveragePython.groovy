2d8dc79d8049c5fe28680420e895a641544bee7d
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 02:39:48 +0530

    Update CoveragePython.groovy

--- src/CoveragePython.groovy
+++ src/CoveragePython.groovy
@@ -2,7 +2,7 @@ def call() {
     sh '''
         python3 -m venv myenv
         . myenv/bin/activate
-        pip install -r requirements.txt
+        pip install -r ${WORKSPACE}/requirements.txt
         pip install coverage
         pip install pytest
         coverage run -m pytest
