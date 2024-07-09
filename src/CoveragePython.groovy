7cd469550956a86e99164ed2a5b914a5e6bc5775
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 03:27:49 +0530

    Update CoveragePython.groovy

--- src/CoveragePython.groovy
+++ src/CoveragePython.groovy
@@ -1,14 +1,19 @@
+// Assuming 'scannerHome', 'projectKey', 'sourcesDir', and 'sonarToken' are defined somewhere in your pipeline or globally.
+
 def call() {
-    sh '''
+    def workspaceDir = "${WORKSPACE}"
+    def pythonEnv = "${workspaceDir}/code-coverage-shared/myenv"
+
+    sh """
         git clone https://github.com/Suryavanshiaditya1999/attendace.git
         cd attendace
-        python3 -m venv myenv
-        . myenv/bin/activate
-        echo "Workspace directory: ${WORKSPACE}/requirments.txt"
-        pip install -r requirements.txt
+        python3 -m venv ${pythonEnv}
+        . ${pythonEnv}/bin/activate
+        echo "Workspace directory: ${workspaceDir}"
+        pip install -r ${workspaceDir}/code-coverage-shared/requirements.txt
         pip install coverage
         pip install pytest
         coverage run -m pytest
         coverage report -m
-    '''
+    """
 }
