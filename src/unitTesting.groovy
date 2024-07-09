e5375b16d723f597f4c6679fb54a9621445a2b05
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 00:00:23 +0530

    Update unitTesting.groovy

--- src/unitTesting.groovy
+++ src/unitTesting.groovy
@@ -1,20 +1,10 @@
-package org.jenkinsci.pipeline
-
-class UnitTesting implements Serializable {
-    def steps
-
-    UnitTesting(steps) {
-        this.steps = steps
-    }
-
-    def setup() {
-        steps.sh '''
-            python3 -m venv venv
-            . venv/bin/activate
-            pip install pytest
-            pip install pytest-html
-            pip install -r requirements.txt
-            python3 -m pytest --continue-on-collection-errors --maxfail=5 --html=report.html
-        '''
-    }
+def call() {
+    sh '''
+        python3 -m venv venv
+        . venv/bin/activate
+        pip install pytest
+        pip install pytest-html
+        pip install -r requirements.txt
+        python3 -m pytest --continue-on-collection-errors --maxfail=5 --html=report.html
+    '''
 }
