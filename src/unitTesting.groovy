3b51c54a413ad9277764a8189b62a40b2160eb71
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 00:13:57 +0530

    Update unitTesting.groovy

--- src/unitTesting.groovy
+++ src/unitTesting.groovy
@@ -1,10 +1,8 @@
 def call() {
-    sh '''
-        python3 -m venv venv
-        . venv/bin/activate
-        pip install pytest
-        pip install pytest-html
-        pip install -r requirements.txt
-        python3 -m pytest --continue-on-collection-errors --maxfail=5 --html=report.html
-    '''
+       sh " python3 -m venv venv " +
+        " . venv/bin/activate " +
+        "pip install pytest " +
+        "pip install pytest-html" +
+        "pip install -r requirements.txt" +
+        "python3 -m pytest --continue-on-collection-errors --maxfail=5 --html=report.html"
 }
