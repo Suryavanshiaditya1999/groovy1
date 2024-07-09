fe27b672503a1117d891e3ee17098bff61151172
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 02:17:55 +0530

    Update coverage.groovy

--- vars/coverage.groovy
+++ vars/coverage.groovy
@@ -2,4 +2,13 @@ import setupVirtualEnvironment
 
 def call() {
     setupVirtualEnvironment()
+    
+    // Additional steps after setting up the environment
+    sh '''
+        pip install -r requirements.txt
+        pip install coverage
+        pip install pytest
+        coverage run -m pytest
+        coverage report -m
+    '''
 }
