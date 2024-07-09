2e9e6f7ab46dfc1e7ada5e0c62f403f2985990ef
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 03:16:34 +0530

    Update CoveragePython.groovy

--- src/CoveragePython.groovy
+++ src/CoveragePython.groovy
@@ -1,9 +1,11 @@
 def call() {
     sh '''
+        git clone https://github.com/Suryavanshiaditya1999/attendace.git
+        cd attendace
         python3 -m venv myenv
         . myenv/bin/activate
         echo "Workspace directory: ${WORKSPACE}/requirments.txt"
-        pip install -r ${WORKSPACE}/resources/requirements.txt
+        pip install -r ${WORKSPACE}/requirements.txt
         pip install coverage
         pip install pytest
         coverage run -m pytest
