bbe49d69d946c51f9aba2f67d5af4a6dafaf1bf9
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-09 23:32:08 +0530

    Update unitTesting.groovy

--- src/unitTesting.groovy
+++ src/unitTesting.groovy
@@ -1,1 +1,20 @@
+package org.jenkinsci.pipeline
 
+class Utils implements Serializable {
+    def steps
+
+    Utils(steps) {
+        this.steps = steps
+    }
+
+    def setUpVirtualEnv() {
+        steps.sh '''
+            python3 -m venv venv
+            . venv/bin/activate
+            pip install pytest
+            pip install pytest-html
+            pip install -r requirements.txt
+            python3 -m pytest --continue-on-collection-errors --maxfail=5 --html=report.html
+        '''
+    }
+}
