bd177d666f3895b007b5cd6be2d2efe891378d7f
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 00:23:41 +0530

    Delete src/unitTesting.groovy

--- src/unitTesting.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-       sh " python3 -m venv venv " +
-        " . venv/bin/activate " +
-        "pip install pytest " +
-        "pip install pytest-html" +
-        "pip install -r requirements.txt" +
-        "python3 -m pytest --continue-on-collection-errors --maxfail=5 --html=report.html"
-}
