10e77a916b250da0c434be425f1cbf1efbd8fc02
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 02:24:48 +0530

    Update setupVirtualEnvironment.groovy

--- src/setupVirtualEnvironment.groovy
+++ src/setupVirtualEnvironment.groovy
@@ -2,5 +2,10 @@ def call() {
     sh '''
         python3 -m venv myenv
         . myenv/bin/activate
+        pip install -r requirements.txt
+        pip install coverage
+        pip install pytest
+        coverage run -m pytest
+        coverage report -m
     '''
 }
